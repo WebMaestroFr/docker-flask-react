f38d301139e5e6510284c1c1133c265bc011a539
Etienne <etienne@visulytix.com>
2019-04-13 10:05:09 +0100

    Implemented Authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,53 @@
+import os
 from flask import Flask
 
-app = Flask(__name__, static_url_path='', static_folder='../build')
+from .auth import auth
+from .auth.extensions import (db as auth_db, jwt as auth_jwt)
 
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+class Config(object):
+    APP_NAME = 'DockerFlask'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'some-secret-string'
+    JWT_SECRET_KEY = 'jwt-secret-string'
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+
+
+def create_app(config=None):
+
+    app = Flask(
+        __name__,
+        static_url_path='',
+        static_folder='../build',
+        instance_relative_config=True
+    )
+    app.config.from_object(Config)
+
+    if config is not None:
+        app.config.from_mapping(config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    auth_db.init_app(app)
+    auth_jwt.init_app(app)
+
+    app.register_blueprint(
+        auth,
+        url_prefix='/auth'
+    )
+
+    @app.before_first_request
+    def create_tables():
+        auth_db.create_all()
+
+    @app.route('/')
+    def index():
+        return app.send_static_file('index.html')
+
+    return app
---
+++ app/auth/__init__.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from flask_restful import Api
+
+from . import models, resources
+from .extensions import jwt
+
+
+auth = Blueprint('auth', __name__)
+
+api = Api(auth)
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
